bd5a070d0b5550a9ccbb7793903ffcb37f754aff
chernysh2909 <chernysh2909@gmail.com>
2016-08-23 12:30:22 +0300

    header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
+"use strict";
 console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+
+	$(".myMenu ul li ").hover(function() {
+        $(this).children("ul").stop(!0, !1, !0).fadeToggle(400)
+    }),
+    // $(".header-top-banner").delay(1000).fadeToggle(500);
+     $(".header-top-banner").delay(1000).addClass('animated fadeIn').css('visibility', 'visible');
+  
+});
+
